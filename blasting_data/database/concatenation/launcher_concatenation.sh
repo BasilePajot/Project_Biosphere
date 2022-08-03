fdd303d75df080e4720d22e7c9a9d0313123d964
BasilePajot <91027316+BasilePajot@users.noreply.github.com>
2022-08-03 16:09:49 +0200

    Delete blasting_data directory

--- blasting_data/database/concatenation/launcher_concatenation.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#SBATCH --job-name=biosphere
-#SBATCH --account=biomics
-#SBATCH --partition=defq
-#SBATCH --time=96:00:00
-#SBATCH --mail-user=basile.pajot@supagro.fr
-#SBATCH --mail-type=end
-
-SINGULARITY_CACHEDIR=/lustre/${USER}/.singularity MPLCONFIGDIR=/lustre/${USER}/ NUMBA_CACHE_DIR=/lustre/${USER} sbatch --export=PATH,SINGULARITY_CACHEDIR,MPLCONFIGDIR,NUMBA_CACHE_DIR /lustre/pajotb/blasting_data/database/concatenation/script_concat.sh
--- blasting_data/database/sorting/script_sort.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#SBATCH --job-name=biosphere
-#SBATCH --account=biomics
-#SBATCH --partition=defq
-#SBATCH --time=96:00:00
-
-Rscript sorting_script.R
